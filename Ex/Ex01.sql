7519aa3a64a18491d9ac6f29c6e7ad507481d1dd
jnnd24 <jnnd24@naver.com>
2022-05-11 16:06:44 +0900

    5/11 select문 기본

--- Ex/Ex01.sql
+++ Ex/Ex01.sql
@@ -1,3 +1,6 @@
 /*select문 기본*/
-select * from emplyees;
-select * from departments;
+select * from employees;
+select *from departments;
+
+/* 한번에 여러개도 불러올 수 있음 ","로 구분해주면 됨 */
+select employee_id, phone_number, first_name, first_name from employees; 
